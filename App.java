99e2f6994001a997e5c0270f9c75647499726ef5
stharajan <123610961+stharajan@users.noreply.github.com>
2023-01-26 22:50:45 -0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+package patterns;
+
+		// Print Solid Rectangle using Nested loop.
+
+public class App {
+	public static void main(String[] args) {
+		
+		int n = 4;
+		int m = 5;
+		
+		// outer loop
+		for (int i=1; i<=n; i++) {
+			// inner loop
+		for (int j=1; j<=m; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	}
+
+}
